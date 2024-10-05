c02ee4c5cd7684d42838e6e797e03ea404bd89fa
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2024-10-05 16:04:46 +0200

    feat: convert to node20 action (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { Glob, file } from 'bun'
+import { readFile as readFileAsync } from 'node:fs/promises'
+import { getBooleanInput, getInput } from '@actions/core'
+import { globIterate } from 'glob'
 import {
   buildSchema,
   type DocumentNode,
@@ -11,31 +13,19 @@ import {
   visitWithTypeInfo,
 } from 'graphql'
 
-const schemaFilePath = process.env.SCHEMA_FILE!
-const operationsGlob = new Glob(process.env.OPERATION_FILES_GLOB!)
-const shouldReportFiles = process.env.REPORT_FILES !== 'false'
-
-const schema = buildSchema(await file(schemaFilePath).text())
+const schemaFilePath = process.env.SCHEMA_FILE || getInput('schema-file', { required: true })
+const operationsGlob = process.env.OPERATION_FILES_GLOB || getInput('operation-files-glob', { required: true })
+const shouldReportFiles = process.env.REPORT_FILES || getBooleanInput('report-files', { required: true })
 
 const fragmentsByName = new Map<string, FragmentDefinitionNode>()
 const dependenciesByFragmentName = new Map<string, Set<string>>()
 
-function listFragmentDependencies(fragment: DocumentNode | FragmentDefinitionNode) {
-  const dependencies = new Set<string>()
-  visit(fragment, {
-    FragmentSpread(node) {
-      dependencies.add(node.name.value)
-    },
-  })
-  return dependencies
-}
-
 const documentsMap = new Map<string, DocumentNode>()
 
-for await (const operationFilePath of operationsGlob.scan('.')) {
+for await (const operationFilePath of globIterate(operationsGlob)) {
   if (operationFilePath === schemaFilePath) continue
 
-  const document = parse(await file(operationFilePath).text())
+  const document = parse(await  readFile(operationFilePath))
   documentsMap.set(operationFilePath, document)
 }
 
@@ -49,10 +39,11 @@ for (const document of documentsMap.values()) {
   })
 }
 
+const schema = buildSchema(await readFile(schemaFilePath))
 const deprecatedFields = new Set<string>()
 
 // second pass, validate operations and fragments
-for (const [operationFilePath, document] of documentsMap) {
+for (const [operationFilePath, document] of documentsMap.entries()) {
   const operationDefinitions = document.definitions.filter((def) => def.kind === Kind.OPERATION_DEFINITION)
 
   if (operationDefinitions.length === 0) continue
@@ -61,10 +52,13 @@ for (const [operationFilePath, document] of documentsMap) {
 
   function recursivelyAddDependencies(fragmentName: string) {
     const dependencies = dependenciesByFragmentName.get(fragmentName)
-    if (!dependencies) return
-    dependencies.forEach((dep) => {
-      usedFragmentsNames.add(dep)
-      recursivelyAddDependencies(dep)
+    if (!dependencies?.size) return
+
+    dependencies.forEach((dependencyName) => {
+      if (usedFragmentsNames.has(dependencyName)) return
+
+      usedFragmentsNames.add(dependencyName)
+      recursivelyAddDependencies(dependencyName)
     })
   }
 
@@ -76,9 +70,8 @@ for (const [operationFilePath, document] of documentsMap) {
 
   usedFragmentsNames.forEach((fragmentName) => {
     const frag = fragmentsByName.get(fragmentName)
-    if (!frag) {
-      throw new Error(`Missing fragment "${fragmentName}" in ${operationFilePath}`)
-    }
+    if (!frag) throw new Error(`Missing fragment "${fragmentName}" in ${operationFilePath}`)
+
     requiredFragmentsDefinitions.add(frag)
   })
 
@@ -93,10 +86,7 @@ for (const [operationFilePath, document] of documentsMap) {
 
   if (errors.length > 0) {
     console.error('Validation errors:')
-    errors.forEach((error) => {
-      console.log('errr', error, typeof error, error instanceof Error, Object.getOwnPropertyNames(error), error.message)
-      console.error(error.message)
-    })
+    errors.forEach((error) => console.error(error.message))
   }
 
   const typeInfo = new TypeInfo(schema)
@@ -118,10 +108,23 @@ for (const [operationFilePath, document] of documentsMap) {
 }
 
 if (deprecatedFields.size > 0) {
-  let msg = 'Deprecated fields found ❌\n'
-  msg += Array.from(deprecatedFields).join('\n')
-  console.error(msg)
+  console.error('Deprecated fields found ❌')
+  deprecatedFields.forEach((field) => console.error(field))
   process.exit(1)
 } else {
   console.log('No deprecated fields found. GG ✅')
+}
+
+function readFile(filePath: string) {
+  return readFileAsync(filePath, { encoding: 'utf-8' })
+}
+
+function listFragmentDependencies(fragment: DocumentNode | FragmentDefinitionNode) {
+  const dependencies = new Set<string>()
+  visit(fragment, {
+    FragmentSpread(node) {
+      dependencies.add(node.name.value)
+    },
+  })
+  return dependencies
 }
